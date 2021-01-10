2a9b1820db4ac7e3fdedbcddc09bde19d30fd4ca
AsenTerziyski <asenterziyski@gmail.com>
2021-01-10 10:34:36 +0200

    Update for for-loops lab.

---
+++ 01JavaBasics/05_ForLoops/src/FL06VowelsSum.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class FL06VowelsSum {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String inputText = scanner.nextLine();
+        int i = 0;
+        int sumTotal = 0;
+
+        for (i = 0; i < inputText.length(); i++) {
+
+            char symbol = inputText.charAt(i);
+            switch (symbol) {
+                case 'a':
+                    sumTotal = sumTotal + 1;
+                    break;
+                case 'e':
+                    sumTotal = sumTotal + 2;
+                    break;
+                case 'i':
+                    sumTotal = sumTotal + 3;
+                    break;
+                case 'o':
+                    sumTotal = sumTotal + 4;
+                    break;
+                case 'u':
+                    sumTotal = sumTotal + 5;
+                    break;
+            }
+        }
+
+        System.out.println(sumTotal);
+    }
+}
