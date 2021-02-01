6debd7fb501294fe504040009d70e697442fc0fe
AsenTerziyski <asenterziyski@gmail.com>
2021-02-01 09:49:11 +0200

    Solving problem 01.

---
+++ 03JavaAdvanced/11DefiningClasses/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    public String brand;
+    public String model;
+    public int horsePower;
+
+    public Car(String brandParam, String modelParam, int horsePowerParam) {
+        brand = brandParam;
+        model = modelParam;
+        horsePower = horsePowerParam;
+
+    }
+
+}
