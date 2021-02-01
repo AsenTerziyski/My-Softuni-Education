54195eb005dce63cfcaaad1acc10d7cc72ab1f94
AsenTerziyski <asenterziyski@gmail.com>
2021-02-01 10:52:30 +0200

    Solving problem 01.

---
+++ 03JavaAdvanced/11DefiningClasses/DC01CarInfo/Car.java
@@ -0,0 +1,15 @@
+package DC01CarInfo;
+
+public class Car {
+    public String brand;
+    public String model;
+    public int horsePower;
+
+    public Car(String brandParam, String modelParam, int horsePowerParam) {
+        brand = brandParam;
+        model = modelParam;
+        horsePower = horsePowerParam;
+
+    }
+
+}
