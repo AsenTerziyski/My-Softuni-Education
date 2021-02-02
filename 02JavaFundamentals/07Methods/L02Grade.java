f743665035a8386a80532277ce185f7f395ca562
AsenTerziyski <asenterziyski@gmail.com>
2021-02-02 10:58:01 +0200

    Solving problem 02.

---
+++ 02JavaFundamentals/07Methods/L02Grade.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class L02Grade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double grade = Double.parseDouble(scanner.nextLine());
+        gradeInWords(grade);
+    }
+
+    private static void gradeInWords(double grade) {
+        //•	2.00 – 2.99 - "Fail"
+        //•	3.00 – 3.49 - "Poor"
+        //•	3.50 – 4.49 - "Good"
+        //•	4.50 – 5.49 - "Very good"
+        //•	5.50 – 6.00 - "Excellent"
+        if (2.00 <= grade && grade <= 2.99) {
+            System.out.println("Fail");
+        } else if (3.00 <= grade && grade <= 3.49) {
+            System.out.println("Poor");
+        } else if (3.50 <= grade && grade <= 4.49) {
+            System.out.println("Good");
+        } else if (4.50<=grade && grade<=5.49) {
+            System.out.println("Very good");
+        } else if (5.50<=grade && grade<=6.00){
+            System.out.println("Excellent");
+        } else {
+            System.out.println("Not valid grade!");
+        }
+    }
+}
