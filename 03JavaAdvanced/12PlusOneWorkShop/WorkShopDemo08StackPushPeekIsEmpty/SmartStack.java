8d48a1f10e77ede6510288f1b45793e5d71bc8eb
AsenTerziyski <asenterziyski@gmail.com>
2021-02-07 10:38:31 +0200

    Adding demo 08.

---
+++ 03JavaAdvanced/12PlusOneWorkShop/WorkShopDemo08StackPushPeekIsEmpty/SmartStack.java
@@ -0,0 +1,58 @@
+package WorkShopDemo08StackPushPeekIsEmpty;
+
+public class SmartStack {
+    private static class Node {
+        int element;
+        Node prev;
+
+        private Node(int element) {
+            this.element = element;
+        }
+    }
+
+    private Node top;
+    private int size;
+
+    public SmartStack() {
+    }
+
+    public void push(int element) {
+        Node newNode = new Node(element);
+        if (this.top == null) {
+            this.top = newNode;
+        } else {
+            newNode.prev = this.top;
+            this.top = newNode;
+        }
+        this.size++;
+    }
+
+    public int peek() {
+        ensureNotEmpty("peek");
+        return this.top.element;
+    }
+
+    public int pop() {
+        ensureNotEmpty("pop");
+        int element = this.top.element;
+        this.top = this.top.prev;
+        this.size--;
+        return element;
+    }
+
+    private void ensureNotEmpty(String operation) {
+        if (isEmpty()) {
+            throw new IllegalStateException("Cannot " + operation + " on empty stack.");
+        }
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+
+}
