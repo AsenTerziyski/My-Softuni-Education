622727cd5ea3449824ae996f954668bd7974f3d5
AsenTerziyski <asenterziyski@gmail.com>
2021-02-09 10:11:45 +0200

    Adding demos.

---
+++ 03JavaAdvanced/14WorkShopXrcs/demoLinkedListForEach/Main.java
@@ -0,0 +1,59 @@
+package demoLinkedListForEach;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CustomLinkedList customLinkedList = new CustomLinkedList();
+//        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+//        System.out.println(customLinkedList.getSize());
+//        System.out.println(customLinkedList.isEmpty());
+//        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+//       // System.out.println(customLinkedList.removeFirst());
+        for (int i = 5; i <10; i++) {
+            customLinkedList.addFirst(i+1);
+        }
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        customLinkedList.forEach(el-> System.out.println(el));
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println(customLinkedList.removeFirst());
+        System.out.println(customLinkedList.getSize());
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println(customLinkedList.removeFirst());
+        System.out.println(customLinkedList.getSize());
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println(customLinkedList.removeFirst());
+        System.out.println(customLinkedList.getSize());
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println(customLinkedList.removeFirst());
+        System.out.println(customLinkedList.getSize());
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println(customLinkedList.removeFirst());
+        System.out.println(customLinkedList.getSize());
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println(customLinkedList.removeFirst());
+        System.out.println(customLinkedList.isEmpty());
+        System.out.println(customLinkedList.getSize());
+
+
+//        System.out.println(customLinkedList.removeFirst());
+//        System.out.println(customLinkedList.get(3));
+//        System.out.println(customLinkedList.get(0));
+//        System.out.println(customLinkedList.get((customLinkedList.getSize() - 1)));
+//        System.out.println(customLinkedList.removeFirst());
+
+//        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+//        System.out.println(customLinkedList.getSize());
+//        System.out.println(customLinkedList.isEmpty());
+//        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+
+
+        // write your code here
+    }
+}
