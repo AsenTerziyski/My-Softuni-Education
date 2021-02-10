fc0bdf424268316470490236e44a96dc8c06d8f6
AsenTerziyski <asenterziyski@gmail.com>
2021-02-10 11:20:04 +0200

    Solving problem 02.

--- 03JavaAdvanced/15Generics/GenericsPrblm02GenericArrayCreator/Main.java
+++ 03JavaAdvanced/15Generics/GenericsPrblm02GenericArrayCreator/Main.java
@@ -1,7 +1,5 @@
 package GenericsPrblm02GenericArrayCreator;
 
-import java.util.Stack;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -10,13 +8,18 @@ public class Main {
         String[] strings = ArrayCreator.create(String.class, 13, "thirteen");
 
         for (Integer integer : array) {
-            System.out.println(integer);
+            //System.out.println(integer);
+            printGen(integer);
         }
 
         for (String string : strings) {
             System.out.println(string);
+            printGen(string);
         }
         System.out.println();
         // write your code here
+    }
+    public static <T> void printGen(T element) {
+        System.out.println(element);
     }
 }
