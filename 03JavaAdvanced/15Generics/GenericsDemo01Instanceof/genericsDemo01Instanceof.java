6ffbac794aa9fa3d05ec1257c967bbb584ead67f
AsenTerziyski <asenterziyski@gmail.com>
2021-02-11 09:18:33 +0200

    Adding demo 01.

---
+++ 03JavaAdvanced/15Generics/GenericsDemo01Instanceof/genericsDemo01Instanceof.java
@@ -0,0 +1,25 @@
+package GenericsDemo01Instanceof;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class genericsDemo01Instanceof {
+    public static void main(String[] args) {
+        List elements = new ArrayList();
+        elements.add("Skid Row");
+        elements.add("Winger");
+        elements.add('a');
+        elements.add('b');
+        elements.add(100);
+        elements.add(5000);
+//        for (Object element : elements) {
+//            System.out.println(element instanceof Integer);
+//            System.out.println(element instanceof String);
+//        }
+        System.out.println();
+        for (int i = 0; i <elements.size(); i++) {
+            System.out.println(elements.get(i) instanceof Integer);
+            System.out.println(elements.get(i) instanceof String);
+        }
+    }
+}
