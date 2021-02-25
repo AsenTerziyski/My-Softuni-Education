343e810c4a247809e82a15100a179200e026d7e0
AsenTerziyski <asenterziyski@gmail.com>
2021-02-25 10:48:18 +0200

    Solving problem 02.

---
+++ 04JavaOOP/01WorkingWithAbstractionLab/Pr02Geometry/Main.java
@@ -0,0 +1,39 @@
+package Pr02Geometry;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Rectangle rectangle = getRectangle(scanner);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            Point2D currentPoint = getPoint2D(scanner);
+            System.out.println(rectangle.contains(currentPoint));
+        }
+    }
+
+    private static Point2D getPoint2D(Scanner scanner) {
+        int[] pointCoordinates = getCoordinates(scanner);
+        int x = pointCoordinates[0];
+        int y = pointCoordinates[1];
+        return new Point2D(x, y);
+    }
+
+    private static Rectangle getRectangle(Scanner scanner) {
+        int[] rectangleCoordinates = getCoordinates(scanner);
+        int bottomLeftX = rectangleCoordinates[0];
+        int bottomLeftY = rectangleCoordinates[1];
+        int topRightX = rectangleCoordinates[2];
+        int topRightY = rectangleCoordinates[3];
+        return new Rectangle(new Point2D(bottomLeftX, bottomLeftY), new Point2D(topRightX, topRightY));
+    }
+
+    private static int[] getCoordinates(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+    }
+}
---
+++ 04JavaOOP/01WorkingWithAbstractionLab/Pr02Geometry/Rectangle.java
@@ -0,0 +1,17 @@
+package Pr02Geometry;
+
+public class Rectangle {
+
+    private Point2D bottomLeft;
+    private Point2D topRight;
+
+    public Rectangle(Point2D bottomLeft, Point2D topRight) {
+        this.bottomLeft = bottomLeft;
+        this.topRight = topRight;
+    }
+
+    public boolean contains(Point2D point) {
+        return point.getX() >= bottomLeft.getX() && point.getX() <= topRight.getX()
+                && point.getY() >= bottomLeft.getY() && point.getY() <= topRight.getY();
+    }
+}
