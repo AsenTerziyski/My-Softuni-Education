32651bdd4938645b3ee7acf14bb66353b2f0f8c7
AsenTerziyski <asenterziyski@gmail.com>
2021-03-03 09:41:21 +0200

    Solving problem 01.

---
+++ 04JavaOOP/04EncapsulationExercise/Pr01ClassBox/Box.java
@@ -0,0 +1,52 @@
+package Pr01ClassBox;
+
+public class Box {
+    private double length;
+    private double width;
+    private double height;
+
+    public Box(double length, double width, double height) {
+        this.setLength(length);
+        this.setWidth(width);
+        this.setHeight(height);
+    }
+
+    private void setLength(double length) {
+        if (isPositiveOrBiggerThanZero(length, "Length")) {
+            this.length = length;
+        }
+    }
+
+    private void setWidth(double width) {
+        if (isPositiveOrBiggerThanZero(width, "Width")) {
+            this.width = width;
+        }
+    }
+
+    private void setHeight(double height) {
+        if (isPositiveOrBiggerThanZero(height, "Height")) {
+            this.height = height;
+        }
+    }
+
+    private boolean isPositiveOrBiggerThanZero(double side, String str) {
+        if (side <= 0) {
+            throw new IllegalArgumentException(str + " cannot be zero or negative");
+        } else {
+            return side > 0;
+        }
+    }
+
+    public double calculateSurfaceArea() {
+        double surfaceArea = 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+        return surfaceArea;
+    }
+
+    public double calculateLateralSurfaceArea() {
+        return 2 * (this.length * this.height + this.width * this.height);
+    }
+
+    public double calculateVolume() {
+        return this.height * this.width * this.length;
+    }
+}
---
+++ 04JavaOOP/04EncapsulationExercise/Pr01ClassBox/Main.java
@@ -0,0 +1,18 @@
+package Pr01ClassBox;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double length = Double.parseDouble(scanner.nextLine());
+        double width = Double.parseDouble(scanner.nextLine());
+        double height = Double.parseDouble(scanner.nextLine());
+        Box box = new Box(length, width,height);
+
+        System.out.println(String.format("Surface Area - %.2f", box.calculateSurfaceArea()));
+        System.out.println(String.format("Lateral Surface Area - %.2f", box.calculateLateralSurfaceArea()));
+        System.out.println(String.format("Volume - %.2f", box.calculateVolume()));
+}
+}
