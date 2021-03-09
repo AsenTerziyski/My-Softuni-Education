c3abea19d7451bee537afddfa56bb627465b262f
AsenTerziyski <asenterziyski@gmail.com>
2021-03-09 09:34:50 +0200

    Solving problem 01.

---
+++ 04JavaOOP/07InterfacesAndAbstractionLab/Pr01CarShop/Car.java
@@ -0,0 +1,12 @@
+package Pr01CarShop;
+
+public interface Car {
+    //public static final Integer TIRES = 4;
+    // може и така да се напише"
+    Integer TIRES = 4;
+    public abstract String getModel();
+    //може без public abstract, защото са подразбират.
+    String getColor();
+    Integer getHorsePower();
+    String countryProduced();
+}
