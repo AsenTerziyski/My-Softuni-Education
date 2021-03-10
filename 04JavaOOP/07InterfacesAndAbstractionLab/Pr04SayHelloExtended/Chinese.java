9a990a30f2928df9bc36cdd60c1821e712601a19
AsenTerziyski <asenterziyski@gmail.com>
2021-03-10 10:56:20 +0200

    Solving problem 04.

---
+++ 04JavaOOP/07InterfacesAndAbstractionLab/Pr04SayHelloExtended/Chinese.java
@@ -0,0 +1,13 @@
+package Pr04SayHelloExtended;
+
+public class Chinese extends BasePerson {
+
+    public Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
---
+++ 04JavaOOP/07InterfacesAndAbstractionLab/Pr04SayHelloExtended/Person.java
@@ -0,0 +1,6 @@
+package Pr04SayHelloExtended;
+
+public interface Person {
+    String getName();
+    String sayHello();
+}
