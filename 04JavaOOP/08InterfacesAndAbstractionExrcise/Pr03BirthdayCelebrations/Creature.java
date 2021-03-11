74a0a80bec83271909555345e3d74f0e0f792734
AsenTerziyski <asenterziyski@gmail.com>
2021-03-11 11:25:39 +0200

    Solving problem 03.

---
+++ 04JavaOOP/08InterfacesAndAbstractionExrcise/Pr03BirthdayCelebrations/Creature.java
@@ -0,0 +1,5 @@
+package Pr03BirthdayCelebrations;
+
+public interface Creature {
+    String getName();
+}
---
+++ 04JavaOOP/08InterfacesAndAbstractionExrcise/Pr03BirthdayCelebrations/Machinable.java
@@ -0,0 +1,5 @@
+package Pr03BirthdayCelebrations;
+
+public interface Machinable {
+    String getModel();
+}
