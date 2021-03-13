2e50605e62337ee1e49270324779a6d17f3e8568
AsenTerziyski <asenterziyski@gmail.com>
2021-03-13 10:44:59 +0200

    Solving problem 03.

---
+++ 04JavaOOP/09PolymorphismLab/Pr03Animals/Animal.java
@@ -0,0 +1,26 @@
+package Pr03Animals;
+
+public abstract class Animal {
+    private String name;
+    private String favouriteFood;
+
+    protected Animal(String name, String favouriteFood) {
+        this.name = name;
+        this.favouriteFood = favouriteFood;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    protected String getFavouriteFood() {
+        return favouriteFood;
+    }
+
+    public String explainSelf() {
+        return "I am "
+                + getName()
+                + " and my favourite food is "
+                + getFavouriteFood();
+    }
+}
